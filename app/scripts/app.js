c9366778937ef36fc177ba96fb33ee052ad0336d
Tajammal Sheikh <tosheikh@gmail.com>
2016-12-14 09:48:51 -0500

    added ui router and navigation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,4 +9,18 @@
  * Main module of the application.
  */
 angular
-  .module('angularbpApp', []);
+  .module('angularbpApp', ['ui.router'])
+  .config(function($stateProvider, $urlRouterProvider){
+  	$urlRouterProvider.otherwise("home");
+  	$stateProvider
+        .state('about',{
+            url:"/about",
+            templateUrl:"views/about.html"
+        })
+        .state('home',{
+            url:"/home",
+            templateUrl:"views/main.html",
+            controller:"MainCtrl"
+        });;
+        
+  });
