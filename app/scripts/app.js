b10e0dc27cf5a6e4a066227f342eaecda5434b78
Tajammal Sheikh <tosheikh@gmail.com>
2016-12-14 09:17:02 -0500

    Master Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularbpApp
+ * @description
+ * # angularbpApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularbpApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularbpApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularbpApp
+ */
+angular.module('angularbpApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
